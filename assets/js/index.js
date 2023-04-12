20862949ce35c86e08542e17394e5e1644169387
hlkgt <haihaioell@gmail.com>
2023-04-13 05:39:38 +0700

    refactoring index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,72 +1,109 @@
+// Select DOM elements
 const createButton = document.querySelector(".create-button");
 const listArea = document.querySelector(".list-area");
-let valueInput = document.querySelector('input[name="todos"]');
+const valueInput = document.querySelector('input[name="todos"]');
 
+// Initialize empty array to hold todos
 let todos = [];
 
-function Todos(name, checked, isDelete) {
+// Constructor function for creating new todo objects
+function TodoItem(name, checked, isDelete) {
   this.name = name;
   this.checked = checked;
   this.isDelete = isDelete;
 }
 
-const removeList = (id) => {
-  let listIndex = document.querySelectorAll(".list-index");
-  if (listIndex.length === 1 && id === "1") {
-    listIndex[id - 1].remove();
+// Function to remove a todo item from the list and from the todos array
+const removeListItem = (id) => {
+  // Select all list items
+  let listItems = document.querySelectorAll(".list-index");
+
+  // If there's only one item left, remove it
+  if (listItems.length === 1 && id === "1") {
+    listItems[id - 1].remove();
   } else {
-    listIndex[id].remove();
+    // Otherwise, remove the item at the specified index
+    listItems[id].remove();
   }
-  todos.splice(todos[id], 1);
+
+  // Remove the todo item from the todos array
+  todos.splice(id, 1);
+
+  // Return the updated todos array
   return todos;
 };
 
+// Event listener for when the create button is clicked
 createButton.addEventListener("click", (e) => {
   e.preventDefault();
-  if (todos.length < 5) {
-    if (valueInput.value.length < 5) {
-      return alert("Input Length  cant under 5 character");
-    } else {
-      let getValue = valueInput.value;
-      let createTodo = new Todos(getValue, false, false);
-      todos.push(createTodo);
-    }
-  } else {
-    alert("Cannot Add Task, u have 5 task in list task");
+
+  // If there are already 5 todos, don't allow another one to be added
+  if (todos.length >= 5) {
+    alert("Cannot add task, you already have 5 tasks in the list.");
+    return;
   }
+
+  // If the input value is less than 5 characters, don't allow a todo to be created
+  if (valueInput.value.length < 5) {
+    alert("Input length cannot be less than 5 characters.");
+    return;
+  }
+
+  // Create a new todo object and add it to the todos array
+  const newTodo = new TodoItem(valueInput.value, false, false);
+  todos.push(newTodo);
+
+  // Clear the input field
   valueInput.value = "";
-  let todo = "";
+
+  // Generate HTML for the todo list
+  let listHTML = "";
   todos.forEach((item, index) => {
-    if (item.isDelete === false) {
-      todo += `<li class="list-index">
-                <div class="task-list">
-                  <p>${item.name}</p>
-                  <button class="complete-button">complete</button>
-                  <button data-id="${index}" class="remove-button">remove</button>
-                </div>
-              </li>`;
+    if (!item.isDelete) {
+      listHTML += `
+        <li class="list-index">
+          <div class="task-list">
+            <p>${item.name}</p>
+            <button class="complete-button">complete</button>
+            <button data-id="${index}" class="remove-button">remove</button>
+          </div>
+        </li>
+      `;
     }
   });
-  listArea.innerHTML = todo;
-  let task = document.querySelectorAll(".task-list");
-  let completeButton = document.querySelectorAll(".complete-button");
-  for (let i = 0; i < completeButton.length; i++) {
-    completeButton[i].addEventListener("click", () => {
-      let confirmation = confirm("complete?");
-      console.log(task);
+
+  // Update the list area with the new HTML
+  listArea.innerHTML = listHTML;
+
+  // Select all task list items and complete buttons
+  const taskItems = document.querySelectorAll(".task-list");
+  const completeButtons = document.querySelectorAll(".complete-button");
+
+  // Add event listeners to each complete button
+  completeButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      // Prompt the user to confirm completion
+      const confirmation = confirm("Complete?");
+
+      // Add or remove the 'checked' class based on user response
       if (confirmation) {
-        task[i].classList.add("checked");
+        taskItems[index].classList.add("checked");
       } else {
-        task[i].classList.remove("checked");
+        taskItems[index].classList.remove("checked");
       }
     });
-  }
-  let removeButton = document.querySelectorAll(".remove-button");
-  for (let i = 0; i < removeButton.length; i++) {
-    removeButton[i].addEventListener("click", (e) => {
+  });
+
+  // Select all remove buttons
+  const removeButtons = document.querySelectorAll(".remove-button");
+
+  // Add event listeners to each remove button
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
       e.preventDefault();
-      let getId = e.target.dataset.id;
-      removeList(getId);
+      // Get the index of the todo to remove from the "data-id" attribute
+      const id = e.target.dataset.id;
+      removeListItem(id);
     });
-  }
+  });
 });
